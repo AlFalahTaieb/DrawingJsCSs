ef19d431722c1701b6f4d2a082dfcbafb38ba624
al falah taieb <mohamedtaieb.alfalah@esprit.tn>
2017-07-10 14:01:35 +0200

    Drawing with HSL color

--- script.js
+++ script.js
@@ -5,27 +5,45 @@ canvas.height = window.innerHeight;
 ctx.strokeStyle = '#BADA55';
 ctx.lineJoin = 'round';
 ctx.lineCap = 'round';
+ctx.lineWidth = 20;
 
 let isDrawing=false;
 let lastX=0;
 let lasY=0;
+let hue=0;
+let direction = true;
 
-function draw(e){
-	// console.log(e);
-	if(!isDrawing) return;
-	ctx.beginPath();
-	ctx.moveTo(lastX,lasY);
-	ctx.lineTo(e.offsetX,e.offsetY);
-	ctx.stroke();
-	[lastX,lastY]=[e.offsetX,e.offsetY];
+function draw(e) {
+  if (!isDrawing) return; // stop the fn from running when they are not moused down
+  console.log(e);
+   ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
+
+  ctx.beginPath();
+  // start from
+  ctx.moveTo(lastX, lastY);
+  // go to
+  ctx.lineTo(e.offsetX, e.offsetY);
+  ctx.stroke();
+  [lastX, lastY] = [e.offsetX, e.offsetY];
+  hue ++;
+  if(hue>=360){
+  	hue=0
+  }
+// if(ctx.lineWidth>=40 || ctx.lineWidth<=10){
+// 	direction=!direction;
+// }
+// if(direction){
+// 	ctx.lineWidth++;
+// }else{
+// 	ctx.lineWidth--;
+// }
 }
 
 
-canvas.addEventListener('mousemove',()=>{
-	isDrawing=true;
-	[lastX,lasY]=[e.offsetX,e.offsetY];
-}
+canvas.addEventListener('mousedown', (e) => {
+  isDrawing = true;
+  [lastX, lastY] = [e.offsetX, e.offsetY];
 });
-canvas.addEventListener('mousedown',()=>isDrawing=true);
-canvas.addEventListener('mouseup',()=>isDrawing=false);
-canvas.addEventListener('mouseout',()=>isDrawing=false);
+canvas.addEventListener('mousemove', draw);
+canvas.addEventListener('mouseup', () => isDrawing = false);
+canvas.addEventListener('mouseout', () => isDrawing = false);
